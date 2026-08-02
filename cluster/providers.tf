8d28ec37a166154735a1b42a27fbdda0380dacce
Haotian Lou <haotian_lou@apple.com>
2026-08-02 16:06:34 +0800

    refactor: Move to unified cluster workspace instead of per-node

--- cluster/providers.tf
+++ cluster/providers.tf
@@ -5,7 +5,7 @@ terraform {
     organization = "loushomelab" # Please change this if your HCP organization name is different
 
     workspaces {
-      name = "homelab-pve-r720"
+      name = "homelab-pve-cluster"
     }
   }
 
@@ -19,7 +19,7 @@ terraform {
 
 provider "proxmox" {
   # Configuration options are best passed via environment variables in HCP Terraform:
-  # PROXMOX_VE_ENDPOINT (e.g. https://192.168.1.x:8006/)
+  # PROXMOX_VE_ENDPOINT (e.g. https://192.168.1.x:8006/) - Suggest using an HA IP/Load Balancer IP or just one stable node
   # PROXMOX_VE_API_TOKEN (e.g. root@pam!terraform=...)
   insecure = true
 }
