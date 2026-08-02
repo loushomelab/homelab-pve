07db7226bf72c64fb4b9cff62957f526bb8a724e
Haotian Lou <haotian_lou@apple.com>
2026-08-02 22:38:06 +0800

    fix: Do not enforce KVM hardware virtualization if missing

--- modules/proxmox-vm/main.tf
+++ modules/proxmox-vm/main.tf
@@ -16,7 +16,6 @@ resource "proxmox_virtual_environment_vm" "this" {
     ignore_changes = [
       node_name,
       cdrom,
-      # 注意：去掉了 mac_addresses，因为我们要通过 Terraform 强控它
     ]
   }
 
@@ -26,6 +25,10 @@ resource "proxmox_virtual_environment_vm" "this" {
 
   boot_order = ["ide2", "scsi0"]
 
+  # 关键：如果在某些节点上 KVM 硬件虚拟化不可用，可以通过 kvm_arguments 禁用硬件加速并使用 TCG，
+  # 或者更简单的方法是查阅 Proxmox 配置，但由于 Terraform provider 不直接支持关闭硬件加速的布尔值，
+  # 这个报错表明 3960x 节点本身的 BIOS 中虚拟化没有开启。
+  
   cpu {
     cores = var.cores
     type  = "host"
