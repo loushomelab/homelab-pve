f9edfe3bb8e83504d0e0b72f77998a974a847cdb
Haotian Lou <haotian_lou@apple.com>
2026-08-03 12:11:49 +0800

    feat: add meaningful ip and mac outputs, remove test output

--- cluster/test.tf
+++
@@ -1,6 +0,0 @@
-# 这是一个测试数据源，用于验证 Terraform 是否能成功连接到 PVE 集群并读取数据
-data "proxmox_virtual_environment_nodes" "cluster_nodes" {}
-
-output "pve_nodes" {
-  value = data.proxmox_virtual_environment_nodes.cluster_nodes.names
-}
--- modules/proxmox-vm/outputs.tf
+++ modules/proxmox-vm/outputs.tf
@@ -6,3 +6,8 @@ output "ipv4_addresses" {
   description = "The IPv4 addresses reported by the QEMU guest agent"
   value       = proxmox_virtual_environment_vm.this.ipv4_addresses
 }
+
+output "mac_address" {
+  description = "The MAC address of the VM"
+  value       = proxmox_virtual_environment_vm.this.network_device[0].mac_address
+}
