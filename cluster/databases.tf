d07b88e87df0e70edef7c50364804576adb54de8
Lou <lou@humblehouse.com.cn>
2026-08-05 18:40:02 +0800

    fix secret names

--- cluster/databases.tf
+++ cluster/databases.tf
@@ -15,43 +15,43 @@ data "doppler_secrets" "db" {
 # --- Auth DB Provider (192.168.50.151) ---
 provider "postgresql" {
   alias    = "auth"
-  host     = "192.168.50.151"
+  host     = data.doppler_secrets.db.map.DB_AUTH_PG__HOST
   port     = 5432
   database = "postgres"
   username = "postgres"
-  password = data.doppler_secrets.db.map.DB_AUTH_PG_PASSWORD
+  password = data.doppler_secrets.db.map.DB_AUTH_PG__PASSWORD
   sslmode  = "disable"
 }
 
 resource "postgresql_role" "authentik" {
   provider = postgresql.auth
-  name     = "authentik"
+  name     = data.doppler_secrets.db.map.AUTHENTIK_POSTGRESQL__USER
   login    = true
-  password = data.doppler_secrets.db.map.AUTHENTIK_DB_PASSWORD
+  password = data.doppler_secrets.db.map.AUTHENTIK_POSTGRESQL__PASSWORD
 }
 
 resource "postgresql_database" "authentik" {
   provider = postgresql.auth
-  name     = "authentik"
+  name     = data.doppler_secrets.db.map.AUTHENTIK_POSTGRESQL__NAME
   owner    = postgresql_role.authentik.name
 }
 
 # --- Obs DB Provider (192.168.50.152) ---
 provider "postgresql" {
   alias    = "obs"
-  host     = "192.168.50.152"
+  host     = data.doppler_secrets.db.map.DB_OBS_PG__HOST
   port     = 5432
   database = "postgres"
   username = "postgres"
-  password = data.doppler_secrets.db.map.DB_OBS_PG_PASSWORD
+  password = data.doppler_secrets.db.map.DB_OBS_PG_ROOT__PASSWORD
   sslmode  = "disable"
 }
 
 resource "postgresql_role" "grafana" {
   provider = postgresql.obs
-  name     = "grafana"
+  name     = data.doppler_secrets.db.map.GRAFANA_PG__USER
   login    = true
-  password = data.doppler_secrets.db.map.DB_OBS_PG_PASSWORD
+  password = data.doppler_secrets.db.map.GRAFANA_PG__PASSWORD
 }
 
 resource "postgresql_database" "grafana" {
@@ -62,9 +62,9 @@ resource "postgresql_database" "grafana" {
 
 resource "postgresql_role" "umami" {
   provider = postgresql.obs
-  name     = "umami"
+  name     = data.doppler_secrets.db.map.UMAMI_PG__USER
   login    = true
-  password = data.doppler_secrets.db.map.DB_UMAMI_PG_PASSWORD
+  password = data.doppler_secrets.db.map.UMAMI_PG__PASSWORD
 }
 
 resource "postgresql_database" "umami" {
