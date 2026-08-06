dfc81c24d826a56f124b961989bf9ffb21472311
Lou <lou@humblehouse.com.cn>
2026-08-06 15:48:25 +0800

    Update backups.tf

--- cluster/backups.tf
+++ cluster/backups.tf
@@ -11,9 +11,9 @@ resource "proxmox_storage_pbs" "pbs_lxc" {
   id          = "pbs-lxc"
   server      = data.doppler_secrets.pbs.map.PBS_HOST
   datastore   = data.doppler_secrets.pbs.map.PBS_DATASTORE
-  username    = data.doppler_secrets.pbs.map.PBS_TOKEN_ID
-  password    = data.doppler_secrets.pbs.map.PBS_TOKEN_SECRET
-  fingerprint = data.doppler_secrets.pbs.map.PBS_FINGERPRINT
+  username    = trimspace(data.doppler_secrets.pbs.map.PBS_TOKEN_ID)
+  password    = trimspace(data.doppler_secrets.pbs.map.PBS_TOKEN_SECRET)
+  fingerprint = trimspace(data.doppler_secrets.pbs.map.PBS_FINGERPRINT)
 
   content = ["backup"]
 }
