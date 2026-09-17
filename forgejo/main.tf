9e7d055216e6d4bc6405ef5e3cfdaf9397bdf5d0
Lou Haotian <lou@humblehouse.com.cn>
2026-09-17 11:18:03 +0800

    fix(forgejo): define homelab-ansible as standard private repository (#2)

--- forgejo/main.tf
+++ forgejo/main.tf
@@ -1,17 +1,13 @@
 # ==============================================================================
-# 🐙 Forgejo Pull Mirror Repositories
+# 🐙 Forgejo Repositories
 # ==============================================================================
 
 resource "gitea_repository" "homelab_ansible" {
-  username                     = "loushomelab"
-  name                         = "homelab-ansible"
-  description                  = "Homelab Ansible Infrastructure (Automated Pull Mirror from GitHub)"
-  private                      = true
-  mirror                       = true
-  migration_mirror_interval    = "1h"
-  migration_service            = "github"
-  migration_clone_address      = "https://github.com/loushomelab/homelab-ansible.git"
-  migration_service_auth_token = data.doppler_secrets.this.map.GITHUB_PAT
+  username    = "loushomelab"
+  name        = "homelab-ansible"
+  description = "Homelab Ansible Infrastructure"
+  private     = true
+  auto_init   = false
 
   has_issues   = false
   has_wiki     = false
