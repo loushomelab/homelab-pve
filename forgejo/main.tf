bbfe40787cade5d5d3f4421c1cf5af8322adc658
Lou Haotian <lou@humblehouse.com.cn>
2026-09-17 11:39:08 +0800

    feat(forgejo): declare dotfiles repository (#3)

--- forgejo/main.tf
+++ forgejo/main.tf
@@ -13,3 +13,15 @@ resource "gitea_repository" "homelab_ansible" {
   has_wiki     = false
   has_projects = false
 }
+
+resource "gitea_repository" "dotfiles" {
+  username    = "loushomelab"
+  name        = "dotfiles"
+  description = "Personal Dotfiles (Managed via Chezmoi)"
+  private     = true
+  auto_init   = false
+
+  has_issues   = false
+  has_wiki     = false
+  has_projects = false
+}
--- forgejo/outputs.tf
+++ forgejo/outputs.tf
@@ -2,3 +2,8 @@ output "homelab_ansible_clone_url" {
   description = "Forgejo internal clone URL"
   value       = gitea_repository.homelab_ansible.clone_url
 }
+
+output "dotfiles_clone_url" {
+  description = "Forgejo internal clone URL for dotfiles"
+  value       = gitea_repository.dotfiles.clone_url
+}
