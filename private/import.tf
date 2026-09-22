aa80ef9800e56cc4f582b735e96a28a6fdd6d756
Lou Haotian <louhaotian128@gmail.com>
2026-09-22 23:41:43 +0800

    Adopt preloaded NixOS ISO

---
+++ private/import.tf
@@ -0,0 +1,4 @@
+import {
+  to = proxmox_download_file.nixos_minimal_iso
+  id = "r720/local:iso/nixos-minimal-26.05.10402.1e8bc658fc98-x86_64-linux.iso"
+}
--- private/main.tf
+++ private/main.tf
@@ -1,7 +1,7 @@
 locals {
-  nixos_release    = "26.05.10304.6d663c0533ff"
+  nixos_release    = "26.05.10402.1e8bc658fc98"
   nixos_iso_url    = "https://releases.nixos.org/nixos/26.05/nixos-${local.nixos_release}/nixos-minimal-${local.nixos_release}-x86_64-linux.iso"
-  nixos_iso_sha256 = "a09d6be915449466c1c72aaeb3296d58f37cbd5c32b3b7bad4e68ee93a153950"
+  nixos_iso_sha256 = "759d63493bea33a2b320c1ec82d1fdf9a73c61c08e037a526bd364294ed9580f"
 }
 
 resource "proxmox_download_file" "nixos_minimal_iso" {
